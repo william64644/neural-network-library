85dd4df5fd70704a579a65e79a8a6a993b817b63
william64644 <william.mine64@gmail.com>
2022-06-01 00:35:43 -0300

    update

--- main.cpp
+++ main.cpp
@@ -14,41 +14,47 @@ int main()
 
 	const char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-	Layer input(3, 4);
-	Layer hidden1(4, 4);
-	Layer hidden2(4, 4);
-	Layer hidden3(4, 3);
+	Layer input(3, 3);
 	Layer output(3, 0);
 
+	/*
+		1 0 0
+		0 1 0
+
+		0 1 0
+		0 0 1
+
+		0 0 1
+		1 0 0
+	*/
+
 	// rock paper scissors
-	input.neurons = {3, 0, 0};
+	input.neurons = {1, 0, 0};
 
 	vector<vector<double>> results;
 
-	for (unsigned int i = 0; i < 100000; i++)
+	for (unsigned int i = 0; i < 2; i++)
 	{
-
 		matrix_randomizer(input.weights);
-		matrix_randomizer(hidden1.weights);
-		matrix_randomizer(hidden2.weights);
-		matrix_randomizer(hidden3.weights);
-		matrix_randomizer(output.weights);
 
-		layer_parser(input, hidden1);
-		layer_parser(hidden1, hidden2);
-		layer_parser(hidden2, hidden3);
-		layer_parser(hidden3, output);
+		layer_parser(input, output);
 
 		results.push_back(output.neurons);
 	}
 	results = matrix_transponser(results);
 
-	cout << '\n'
-		 << average(results[0]);
-	cout << '\n'
-		 << average(results[1]);
-	cout << '\n'
-		 << average(results[2]);
+	Vector3 error = {(average(results[0])) * -1, (average(results[1]) - 1) * -1, (average(results[2])) * -1};
+
+	vector<double>
+		averages = {average(results[0]), average(results[1]), average(results[2])};
+
+	cout << averages[0] << " error: " << error.x << '\n';
+	cout << averages[1] << " error: " << error.y << '\n';
+	cout << averages[2] << " error: " << error.z << '\n';
+
+	vector_packager(averages, "data/neurons/B");
+	repack(input.weights, "data/weights/AB");
+
 	return 0;
 }
-// sssssssssssssssssss
+// ssssssssssssssssssssss
