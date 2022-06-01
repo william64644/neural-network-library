73400d7256e5a427f1cedad9ace6589c3c2bd4ff
William <59394187+william64644@users.noreply.github.com>
2022-06-01 13:11:25 -0300

    update

--- run_codeV4.bash
+++ run_codeV4.bash
@@ -2,18 +2,17 @@
 
 clear
 
-touch previous_hash
-touch compiled
-chmod +x compiled
+sudo touch previous_hash
+sudo touch compiled
 
 current_hash=`sha1sum main.cpp`
 
 previous_hash=`cat previous_hash`
 
 if [[ "$current_hash" == "$previous_hash" ]]; then
-    ./compiled
+    sudo ./compiled
 else
-    g++ main.cpp -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused -o compiled
+    sudo g++ main.cpp -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused -o compiled
     
     echo "$current_hash" > previous_hash
 fi
