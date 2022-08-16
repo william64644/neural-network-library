4186e950acf2bfca9c91e055a0bc7d9f13ef4596
William <59394187+william64644@users.noreply.github.com>
2022-08-16 11:54:37 -0300

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 //#include <SFML/Graphics.hpp>
 #include <cmath>
 #include "headers/global.h"
+#include "headers/repack_network.h"
 
 // ssssssssssssssssssssss
 // output.shrink_to_fit();
@@ -13,39 +14,34 @@ using namespace std;
 int main()
 {
 	// Create network
-	Layer input(2, 8, "Input", {"Temperature", "Pressure"});
-	Layer hidden1(8, 1, "Hidden1");
-	Layer hidden2(8, 1, "Hidden2");
-	Layer output(1, 0, "Output", {"Solid", "Liquid", "Gas"});
+	Layer input(3, 3, "Input", {"Rock", "Paper", "Scissor"});
+	Layer output(3, 0, "Output", {"Rock", "Paper", "Scissor"});
 
-	Network network({input, hidden1, hidden2, output});
+	Network network({input, output});
 
 	matrix_randomizer(network.layers[0].weights);
-	matrix_randomizer(network.layers[1].weights);
-	
+
 	vector<vector<vector<double>>> labeled_in_out = {
-			{{0.1,0.1},{0.50}},
-			{{1,0.1},{0.1}},
-			{{0.1,1},{1}},
-			{{1,1},{0.50}}
+			{{1,0,0},{0,1,0}},
+			{{0,1,0},{0,0,1}},
+			{{0,0,1},{1,0,0}},
 		};
 
-	network.run_network();
-
+	repack_network(network, "networks/untrained_");
+	
 	// Train
-	const double TRAINING_RATE = 0.01;
-	const double TRAIN_ITERATIONS = 1;
-
-	// Train
-	Network trained_network = get_funneled_trained_network(network, labeled_in_out, 30, 30000);
-		
+	Network trained_network = get_funneled_trained_network(network, labeled_in_out, 30, 40000);
+	
+	repack_network(trained_network, "networks/trained_");
+	
+	// Runtime
 	while (true)
 	{
-		vector<double> game_input = ask_network_input_from_user(2);
+		vector<double> game_input = ask_network_input_from_user(3);
 		trained_network.layers[0].neurons = game_input;
 		trained_network.run_network();
 		trained_network.print_network();
 	}
 	return 0;
 }
-// ssssssssssssssssssssssss
+// sssssssssssssssssssssssss
