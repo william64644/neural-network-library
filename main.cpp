53e267581de80fe1e91bf588773d6a92b5f668f2
William <59394187+william64644@users.noreply.github.com>
2022-08-17 08:28:39 -0300

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <cmath>
 #include "headers/global.h"
 #include "headers/repack_network.h"
+#include "headers/print_matrix.h"
+#include "headers/print_vector.h"
 
 // ssssssssssssssssssssss
 // output.shrink_to_fit();
@@ -28,20 +30,30 @@ int main()
 		};
 
 	repack_network(network, "networks/untrained_");
-	
+//
+	//print_vector(labeled_in_out[0][0]);
 	// Train
-	Network trained_network = get_funneled_trained_network(network, labeled_in_out, 30, 40000);
+	
+	Network trained_network = get_funneled_trained_network(network, labeled_in_out, 2, 40000);
 	
 	repack_network(trained_network, "networks/trained_");
 	
-	// Runtime
-	while (true)
+	trained_network.run_network();
+	trained_network.print_network();
+	
+	// Looping through labeled in/outs
+	for (int labeled_in_out_indice = 0; labeled_in_out_indice < labeled_in_out.size(); labeled_in_out_indice ++)
 	{
+		trained_network.expected_output = labeled_in_out[labeled_in_out_indice][1];
+		
 		vector<double> game_input = ask_network_input_from_user(3);
 		trained_network.layers[0].neurons = game_input;
 		trained_network.run_network();
 		trained_network.print_network();
 	}
+	
+	// Runtime
+	
 	return 0;
 }
-// sssssssssssssssssssssssss
+// sssssssssssssssssssssssssss
